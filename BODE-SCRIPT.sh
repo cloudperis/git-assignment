c5973fa0a67eaa69247a69316679edf334e1b3e5
Olubode Job <bodeige81@gmail.com>
2023-02-09 18:54:14 -0600

    Updated by bode

---
+++ BODE-SCRIPT.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#touch commnad to create empty file
+touch bode-script.txt
+
+#Echo command to add content in a file
+echo "Page 1: The begining of the movie" > bode-script.txt
+echo "Page 2: The end of the movie" >> bode-script.txt
+
+
+
+
